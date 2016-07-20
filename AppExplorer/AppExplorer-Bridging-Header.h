1a136679a7d419614818b01169b941a5059715a5
Cardasis <jcardasi@MGC000001684>
2016-07-20 09:22:56 -0400

    Folder Cleanup

---
+++ AppExplorer/AppExplorer-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "LSApplicationProxy.h"
+#import "LSApplicationWorkspace.h"
+#import "UIImage-ApplicationIcons.h"
+
+#import "DirectoryReader.h"
